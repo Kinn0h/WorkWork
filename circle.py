6d9903b9d460f9345975f54d4f0262dbf8aeb68e
Mikhail <mikhailkondakoov@gmail.com>
2024-12-12 09:29:45 +0300

    documentation circle.py square.py

--- circle.py
+++ circle.py
@@ -2,9 +2,18 @@ import math
 
 
 def area(r):
+    '''takes the radius of a circle
+        returns the area
+        example of usage:
+        area(10) = 314.1592653589793'''
     return math.pi * r * r
 
 
-def perimeter(r):
-    return 2 * math.pi * r
 
+
+def perimeter(r):
+    '''takes the radius of a circle
+        returns the perimeter
+        example of usage:
+        area(10) = 62.83185307179586'''
+    return 2 * math.pi * r
--- square.py
+++ square.py
@@ -1,7 +1,15 @@
-
 def area(a):
+    '''takes the side of a square
+        returns the area
+        example of usage:
+        area(3) = 9'''
     return a * a
 
 
+
 def perimeter(a):
+    '''takes the side of a square
+        returns the perimeter
+        example of usage:
+        perimeter(3) = 12'''
     return 4 * a
