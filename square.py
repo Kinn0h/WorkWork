55e69b35884dde8022e8fe8fda26bc59d1cb797e
Mikhail <mikhailkondakoov@gmail.com>
2024-12-12 09:32:20 +0300

    documentation circle.py square.py new version

--- square.py
+++ square.py
@@ -1,15 +1,19 @@
 def area(a):
-    '''takes the side of a square
-        returns the area
-        example of usage:
-        area(3) = 9'''
+    '''
+    takes the side of a square
+    returns the area
+    example of usage:
+    area(3) = 9
+    '''
     return a * a
 
 
 
 def perimeter(a):
-    '''takes the side of a square
-        returns the perimeter
-        example of usage:
-        perimeter(3) = 12'''
+    '''
+    takes the side of a square
+    returns the perimeter
+    example of usage:
+    perimeter(3) = 12
+    '''
     return 4 * a
